1ad74c5daf7c5b855382a8a3f53edb6c4d524011
shai2kha2-bit <shai2kha2@gmail.com>
2026-04-16 17:21:02 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   output: 'standalone',
   trailingSlash: true,
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' }
+    ]
+  }
 }
 
 module.exports = nextConfig
